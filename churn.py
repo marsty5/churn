969126b17e65b0fc611a4525b0cd8036e3c319cb
aecc <alessandroaecc@gmail.com>
2014-08-28 23:58:33 +0200

    functions

--- churn.py
+++ churn.py
@@ -2,4 +2,18 @@ import pandas as pd
 
 customerDf = pd.read_csv('datasets/churn.data.simple.noDot')
 
+# Create the decision tree model
+def create_tree(customerDf):
+
+    attributes = customerDf.columns
+    information_gains = map(lambda x: information_gain(x,customerDf),attributes)
+
+
+# Calculate the information gain of one attribute
+def information_gain(attribute, customerDf):
+    # TODO
+    return 0.0
+
+
+
 
