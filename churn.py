35915dd93276cc96036d5d4463cda6ffd6f911d4
aecc <alessandroaecc@gmail.com>
2014-08-29 02:59:28 +0200

    tree creation done, but without output

--- churn.py
+++ churn.py
@@ -1,19 +1,40 @@
 import pandas as pd
+import numpy
 
 customerDf = pd.read_csv('datasets/churn.data.simple.noDot')
+del customerDf['state']
+del customerDf['area_code']
+del customerDf['international_plan']
+del customerDf['voice_mail_plan']
 
 # Create the decision tree model
-def create_tree(customerDf):
+def create_tree(df):
 
-    attributes = customerDf.columns
-    information_gains = map(lambda x: information_gain(x,customerDf),attributes)
+    return split(df)
 
+def split(df):
+
+    if len(df)==0:
+        return []
+    attributes = df.columns.drop('churn')
+    if len(attributes)==0: 
+        return []
+
+    information_gains = map(lambda x: information_gain(x,df),attributes)
+
+    idx = information_gains.index(max(information_gains))
+    best_attribute = attributes[idx]
+    
+    att_mean = df[best_attribute].mean()
+    filteredDfLeft = df[df[best_attribute]<att_mean]
+    filteredDfRight = df[df[best_attribute]>=att_mean]
+    del filteredDfLeft[best_attribute]
+    del filteredDfRight[best_attribute]
+
+    return [best_attribute] + split(filteredDfLeft) + split(filteredDfRight)
+    #return best_attribute
 
 # Calculate the information gain of one attribute
-def information_gain(attribute, customerDf):
+def information_gain(attribute, df):
     # TODO
     return 0.0
-
-
-
-
